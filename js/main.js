249e0e5c6d794524bc8f1708b42e79d9a45e1db7
Random003 <mealla.jp@gmail.com>
2022-07-01 20:27:46 -0300

    agregado js y maquetado pagina web para incluir ejemplo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let totalPago = 0;
+
+let nombreCliente = prompt("Ingrese el nombre del Locador");
+
+let cantidadProp = parseInt(prompt("Ingrese la cantidad de propiedades a nombre del Locador"));
+
+for (let i = 0; i < cantidadProp; i++) {
+
+    let propiedad = prompt("Ingrese la calle de la propiedad a realizar el pago");
+    let precioPago = parseFloat(prompt("Ingrese el valor del alquiler mensual"));
+    totalPago += precioPago;
+    let mensaje = "Ingresó un pago sobre la propiedad en calle " + propiedad + " por un valor de $" + precioPago + ".-";
+    alert(mensaje);
+    console.log(mensaje);
+    
+}
+
+alert ("Al Locador " + nombreCliente + " se le debe depositar un total de $" + totalPago + ".- por el cobro de alquileres.");
