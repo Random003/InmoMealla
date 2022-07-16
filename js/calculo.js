6ff11396a440f938f253f1e5052debfba9563eef
Random003 <mealla.jp@gmail.com>
2022-07-16 17:16:40 -0300

    arreglos y agregados para entregable

--- js/calculo.js
+++ js/calculo.js
@@ -1,4 +1,9 @@
-let totalPago = 0;
+/* let locador = document.getElementById("locador"); */
+
+
+
+
+/* let totalPago = 0;
 
 let nombreCliente = prompt("Ingrese el nombre del Locador");
 
@@ -17,5 +22,45 @@ for (let i = 0; i < cantidadProp; i++) {
     
 }
 
-alert ("Al Locador " + nombreCliente + " se le debe depositar un total de $" + totalPago + ".- por el cobro de alquileres. Los detalles de los inmuebles son los siguientes: " + JSON.stringify(resultado, null, 2)/* .replace("{", " ").replace("}", " ").replace("{", " ").replace("}", " ") */);
+alert ("Al Locador " + nombreCliente + " se le debe depositar un total de $" + totalPago + ".- por el cobro de alquileres. Los detalles de los inmuebles son los siguientes: " + JSON.stringify(resultado, null, 2)/* .replace("{", " ").replace("}", " ").replace("{", " ").replace("}", " ") *//* ); */
+
+
+
+/* -------
+ */
+
+/* let locador = document.getElementById("locador"); */
+
+
+let calcRes = document.getElementById("calcRes");
+
+let totalPago = 0;
+
+let nombreCliente = prompt("Ingrese el nombre del Locador");
+
+let cantidadProp = parseInt(prompt("Ingrese la cantidad de propiedades a nombre del Locador"));
+
+const resultado= [] 
+
+if (cantidadProp !== 0) {
+
+    for (let i = 0; i < cantidadProp; i++) {
+
+        let propiedad = prompt("Ingrese la calle de la propiedad a realizar el pago");
+        let precioPago = parseFloat(prompt("Ingrese el valor del alquiler mensual"));
+        totalPago += precioPago;
+        let mensaje = "Ingresó un pago sobre la propiedad en calle " + propiedad + " por un valor de $" + precioPago + ".-";
+        resultado.push ({calle: propiedad, precio: "$" + precioPago})
+        alert(mensaje);
+        
+    }
+    
+    calcRes.innerHTML = ("Al Locador " + nombreCliente + " se le debe depositar un total de $" + totalPago + ".- por el cobro de alquileres. Los detalles de los inmuebles son los siguientes: " + JSON.stringify(resultado, null, 2));
+    
+
+} else {
+    alert ("Revise sus resultados por favor e intente nuevamente presionando ok");
+}
+
+
 
