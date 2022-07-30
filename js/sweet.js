7c2075091ad5c2752a6d475a33aea586926ddb72
Random003 <mealla.jp@gmail.com>
2022-07-30 17:29:15 -0300

    arreglos finales envio desafio

--- js/sweet.js
+++ js/sweet.js
@@ -1,34 +1,36 @@
 
-
-
 let boton = document.getElementById("botCont");
 
-/* boton.addEventListener("click", () => {
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-      
-      Toast.fire({
-        icon: 'success',
-        title: 'Signed in successfully'
-      })
-});
- */
-
-
 boton.addEventListener("click", () => {
+  swal({
+    title: "Formulario Enviado",
+    text: "Gracias por comunicarse con nosotros, en breve le responderemos",
+    icon: "success",
+    button: "Continuar",
+  });
+});
+ 
+
+
+let botonn = document.getElementById("botLimp");
+
+botonn.addEventListener("click", () => {
+  swal({
+    title: "Campos Borrados",
+    text: "Puede llenarlos nuevamente",
+    icon: "error",
+    button: "Continuar",
+  });
+});
+
+
+
+
+/* boton.addEventListener("click", () => {
 
   Noty({
     type: 'success',
     layout: 'topRight',
     text: 'Some notification text'
 }).show();
-});
+}); */
